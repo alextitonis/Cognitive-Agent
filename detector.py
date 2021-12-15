ee87a0e3a6acde19da34f61ca2f855f0d340818a
alextitonis <alexandrostitonis@gmail.com>
2021-12-15 18:04:07 +0200

    put the detector and trainer into classes

--- detector.py
+++ detector.py
@@ -3,19 +3,21 @@ import os
 from PIL import Image
 from numpy import asarray
 
-img = Image.open('image.jpeg')
-data = asarray(img)
-
-execution_path = os.getcwd()
-
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=data, input_type="array", output_image_path="imagenew.jpg", minimum_percentage_probability=50)
-
-for eachObject in detections:
-    print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-    x1, y1, x2, y2 = eachObject["box_points"]
-    center = (x1 + x2) / 2, (y1 + y2) / 2
-    print("--------------------------------")
+class detector:
+    def __init__(self, modelPath="resnet50_coco_best_v2.1.0.h5"):
+        self.detector = ObjectDetection()
+        self.detector.setModelTypeAsRetinaNet()
+        self.detector.setModelPath(modelPath)
+        self.detector.loadModel()
+        
+    def detect(self, image_data, image_size, input_type="array", output_image_path="imagenew.jpg", minPerc=50):
+        #img = Image.open('test.jpg')
+        #data = asarray(img)
+        #get the size of the image to get the center based on the screen space
+        detections = self.detector.detectObjectsFromImage(input_image=image_data, input_type=input_type, output_image_path=output_image_path, minimum_percentage_probability=minPerc)
+    
+        for eachObject in detections:
+            print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+            x1, y1, x2, y2 = eachObject["box_points"]
+            center = (x1 + x2) / 2, (y1 + y2) / 2
+            print("--------------------------------")
--- detectorTrainer.py
+++ detectorTrainer.py
@@ -1,9 +1,13 @@
 from imageai.Classification.Custom import ClassificationModelTrainer
 import os
 
-execution_path = os.getcwd()
-
-model_trainer = ClassificationModelTrainer()
-model_trainer.setModelTypeAsResNet50()
-model_trainer.setDataDirectory(execution_path + '/custom_model')
-model_trainer.trainModel(num_objects=10, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
+class detectorTrainer:
+    def __init__(self, modelPath, num_objects=10, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True):
+        self.model_trainer = ClassificationModelTrainer()
+        self.model_trainer.setModelTypeAsResNet50()
+        self.model_trainer.setDataDirectory(modelPath)
+        self.model_trainer.trainModel(num_objects=num_objects, 
+                                      num_experiments=num_experiments, 
+                                      enhance_data=enhance_data, 
+                                      batch_size=batch_size, 
+                                      show_network_summary=show_network_summary)
